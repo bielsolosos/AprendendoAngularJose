5b48f69e401574d8d15699da9bb80aa934561e00
Gabriel Coutinho <125703161+bielsolosos@users.noreply.github.com>
2024-11-04 17:51:45 -0300

    Agora renderiza automático

--- src/app/zeCoisas/ze/ze.component.ts
+++ src/app/zeCoisas/ze/ze.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { JoseTeste } from '../../Services/ze';
 import { ZeComponentService } from '../../Services/ze-component.service';
 
@@ -7,12 +7,24 @@ import { ZeComponentService } from '../../Services/ze-component.service';
   templateUrl: './ze.component.html',
   styleUrl: './ze.component.scss',
 })
-export class ZeComponent {
+export class ZeComponent implements OnInit {
   service = inject(ZeComponentService);
 
   taFunfando = false;
 
   dados: Array<JoseTeste> = [];
+
+  ngOnInit(): void {
+    this.service.listAll().subscribe({
+      next: (data: JoseTeste[]) => {
+        this.dados = data; // Agora você está atribuindo os dados reais
+        console.log(this.dados);
+      },
+      error: (err) => {
+        console.error('Erro ao carregar os dados:', err);
+      },
+    });
+  }
 
   trocar(): any {
     this.taFunfando = !this.taFunfando;
